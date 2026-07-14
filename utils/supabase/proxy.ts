ff050d1065ec9b3fa538269e2011a254ab7cee0b
starziahope <63357058+starziahope@users.noreply.github.com>
2026-07-14 16:35:59 +0900

    Supabase MCP 설정

---
+++ utils/supabase/proxy.ts
@@ -0,0 +1,41 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const updateSession = async (request: NextRequest) => {
+  // Create an unmodified response
+  let supabaseResponse = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+          supabaseResponse = NextResponse.next({
+            request,
+          })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          )
+        },
+      },
+    },
+  );
+
+  // Refreshes the session cookie if needed. Required for keeping the
+  // session valid across Server Components, which cannot set cookies.
+  await supabase.auth.getUser();
+
+  return supabaseResponse
+};
